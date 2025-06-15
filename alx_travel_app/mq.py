8ec3864585beacc317fbb158ac8df33bd714800a
briankvin <briankvin@gmail.com>
2025-06-16 01:23:51 +0300

    Updates

--- alx_travel_app/mq.py
+++
@@ -1,8 +0,0 @@
-from django_rabbitmq.mq import RabbitMQ
-
-class CustomModel(RabbitMQ):
-
- def callback(self, ch, method, properties, body):
-     print("[django-rabbitmq] Received %r" % body)
-     
-# RabbitMQ.callback = CustomModel.callback
